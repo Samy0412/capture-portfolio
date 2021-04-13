78188cad4d98b65836b9537f78e8d3e82228d902
Samy0412 <samantha.gadet@gmail.com>
2021-04-13 00:21:46 -0400

    styled components

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,36 +1,71 @@
 import React from "react";
 
+//styles
+import styled from "styled-components";
+
 function AboutSection() {
   return (
-    <div>
-      <div className="description">
+    <About>
+      <Description>
         <div className="title">
-          <div className="hide">
+          <Hide>
             <h2>We work to make</h2>
-          </div>
-          <div className="hide">
+          </Hide>
+          <Hide>
             <h2>
               your <span>dreams</span> come
             </h2>
-          </div>
-          <div className="hide">
+          </Hide>
+          <Hide>
             <h2>true.</h2>
-          </div>
+          </Hide>
         </div>
         <p>
           Contacts us for any photography ideas that you have. We have
           professionals with amazing skills.
         </p>
         <button>Contact us</button>
-      </div>
-      <div className="image">
+      </Description>
+      <Image>
         <img
           src="https://raw.githubusercontent.com/developedbyed/the-creative-react-course-capture/master/src/img/home1.png"
           alt="guy with a camera"
         />
-      </div>
-    </div>
+      </Image>
+    </About>
   );
 }
+
+//styled components
+const About = styled.div`
+  min-height: 90vh;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 5rem 10rem;
+  color: white;
+`;
+
+const Description = styled.div`
+  flex: 1;
+  padding-right: 5rem;
+  h2 {
+    font-weight: lighter;
+  }
+`;
+
+const Image = styled.div`
+  flex: 1;
+  overflow: hidden;
+  img {
+    width: 100%;
+    height: 80vh;
+    object-fit: cover;
+  }
+`;
+
+const Hide = styled.div`
+  overflow: hidden;
+`;
 
 export default AboutSection;
